9f4881010a41b213022fa9a69ec0dbed6fbbacfe
Bryan Mancilla <70993543@certus.edu.pe>
2022-10-26 10:07:11 -0500

    creacion del proyecto
    subida inicial

---
+++ proyecto/web-miguelo2/src/main/java/pe/com/miguelo/WebMiguelo2Application.java
@@ -0,0 +1,13 @@
+package pe.com.miguelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMiguelo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMiguelo2Application.class, args);
+    }
+
+}
